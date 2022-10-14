490efd9e63853c91685367583cd8e85118305369
JanoBourian <supermath33@icloud.com>
2022-10-13 19:41:33 -0500

    Test

---
+++ programacion_net_II/U!_A2/Ejemplo/Paquete.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+
+namespace Ejemplo
+{
+    class Paquete
+    {
+        public string tipo;
+        public string nombre;
+
+        public Paquete(string nombre)
+        {
+            this.nombre = nombre;
+            this.tipo = "caja";
+        }
+
+        public Paquete(string nombre, string tipo)
+        {
+            this.nombre = nombre;
+            this.tipo = tipo;
+        }
+
+        public void DeterminarDestino(string[] rutas)
+        {
+            Console.Write("Pasará por: ");
+            for(int i = 0; i < rutas.Length; i++)
+            {
+                Console.Write("{0} ", rutas[i]);
+            }
+        }
+
+        public void DeterminarDestino(string rutas)
+        {
+            Console.Write("Pasará por: {0}", rutas);
+        }
+
+    }
+}
---
+++ programacion_net_II/U!_A2/Ejemplo/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Ejemplo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Paquete paquete_basico = new Paquete("Teclado");
+            Paquete paquete_complejo = new Paquete("Televisión", "Contenedor");
+
+            string[] destinos = { "Valladolid", "Mérida", "Plata del Carmen" };
+            paquete_basico.DeterminarDestino(destinos);
+            Console.WriteLine();
+            paquete_complejo.DeterminarDestino("CDMX");
+            Console.ReadLine();
+        }
+    }
+}
